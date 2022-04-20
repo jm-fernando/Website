ba236b7cccba3cb8b5142ed345f73526b4e712b9
jm-fernando <jmfernando@dons.usfca.edu>
2022-04-19 23:29:40 -0700

    Page overhaul p2

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,20 +1,10 @@
-// init ScrollMagic Controller
-var controller = new ScrollMagic.Controller();
-
-// Scene Handler
-var scene1 = new ScrollMagic.Scene({
-  triggerElement: "#page-2", // point of execution
-  duration: $(window).height() - 100, // pin element for the window height - 1
-  triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
-  reverse: true // allows the effect to trigger when scrolled in the reverse direction
-})
-.setPin("#page-2") // the element we want to pin
-.addTo(controller);
-
-// Scene2 Handler
-var scene2 = new ScrollMagic.Scene({
-  triggerElement: "#page-3", // point of execution
-  duration: 150 // pin the element for a total of 400px
-})
-.setPin("#page-3") // the element we want to pin
-.addTo(controller);
+$(function() {
+    var controller = new ScrollMagic.Controller();
+    var containerScene = new ScrollMagic.Scene({
+        triggerElement: '#page-2',
+        duration: 500
+    })
+    .setPin('#page-2')
+    .addIndicators()
+    .addTo(controller);
+});
